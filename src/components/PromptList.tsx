0819371f4a155b1fb91ac9b51ebbc5007f1322bc
Derrick Treul <derrick.treul@gmail.com>
2024-05-18 22:09:35 -0700

    fix spacing

--- src/components/PromptList.tsx
+++ src/components/PromptList.tsx
@@ -14,7 +14,12 @@ const PromptList: React.FC<Props> = (props) => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen space-y-4'>
       {props.prompts.slice(0, currentPromptIndex + 1).map((prompt, index) => (
-        <Prompt key={index} promptText={prompt.promptText} type={prompt.type} onButtonClick={() => setCurrentPromptIndex((prevPromptIndex) => prevPromptIndex + 1)} />
+        <Prompt
+          key={index}
+          promptText={prompt.promptText}
+          type={prompt.type}
+          onButtonClick={() => setCurrentPromptIndex((prevPromptIndex) => prevPromptIndex + 1)}
+        />
       ))
       }
     </div>
