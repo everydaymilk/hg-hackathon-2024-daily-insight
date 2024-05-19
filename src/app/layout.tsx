bed069e034200eb6175589c8e5b79ca24f197d4a
Derrick Treul <derrick.treul@gmail.com>
2024-05-18 22:35:15 -0700

    Update title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "HG 2024 Hackathon - Journal",
-  description: "Journaling web app for HG 2024 Hackathon",
+  title: "HG 2024 Hackathon - Daily Insight",
+  description: "Introspection web app for HG 2024 Hackathon",
 };
 
 export default function RootLayout({
