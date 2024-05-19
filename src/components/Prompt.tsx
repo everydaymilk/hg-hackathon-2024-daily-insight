279fa25d2878265fc5714245ad9abb83c1f346ce
Derrick Treul <derrick.treul@gmail.com>
2024-05-18 22:32:16 -0700

    Add basic styling to button

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -8,12 +8,16 @@ interface Props {
   onButtonClick?: () => void;
 }
 
+
 const Prompt: React.FC<Props> = (props) => {
 
   const [promptComplete, setPromptComplete] = useState(false);
   const [response, setResponse] = useState("");
   const buttonRef = useRef<HTMLButtonElement>(null);
   const userResponseRequired = props.type === "input"
+
+  const enabledButtonClass = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+  const disabledButtonClass = "text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-3 py-1.5 text-center"
 
   // Override so that pressing Enter without Shift will submit the response
   const handleKeyDown = (event: React.KeyboardEvent) => {
@@ -40,6 +44,7 @@ const Prompt: React.FC<Props> = (props) => {
       </div>
       <button
         ref={buttonRef}
+        className={promptComplete || (response === "" && userResponseRequired) ? disabledButtonClass : enabledButtonClass}
         hidden={promptComplete}
         disabled={response === "" && userResponseRequired}
         onClick={() => {
